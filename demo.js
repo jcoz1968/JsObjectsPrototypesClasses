c27f36c2b519f0fce95229de041a64ec84440d0e
Jeffery Cosby <jcoz1968@gmail.com>
2019-11-17 15:38:42 -0600

    assign and immutability

--- demo.js
+++ demo.js
@@ -1,16 +1,36 @@
 "use strict";
 (function() {
-  let person = {
+  let person1 = {
     firstName: "Jeffery",
     lastName: "Cosby",
     age: 18,
     isAdult() {
-      return this.aage >= 18;
+      return this.age >= 18;
     }
   };
-  for (let propertyName in person) {
-    display(propertyName);
+
+  let healthStats = {
+    height: 68,
+    weight: 150
+  };
+
+  function mergeHealthStats(person, healthStats) {
+    return Object.assign({}, person1, healthStats);
   }
+
+  let mergedPerson = mergeHealthStats(person1, healthStats);
+  
+  display(mergedPerson);
+
+  display(person1);
+
+  // let person2 = {};
+  // Object.assign(person2, person1);
+  // display(person2);
+  // display(person1 === person2);
+  // for (let propertyName in person) {
+  //   display(propertyName);
+  // }
   // for...in
   // display(Object.keys(person));
   // display(person.isAdult());
