8c8f830c5e681c87e29cbf3c578258caec6e56f0
Jeffery Cosby <jcoz1968@gmail.com>
2019-11-17 16:09:47 -0600

    using the enumerable attribute

--- demo.js
+++ demo.js
@@ -1,12 +1,36 @@
 "use strict";
-(function () {
-  
+(function() {
   let person = {
-    firstName: 'Jeffery',
-    lastName: 'Cosby',
+    firstName: "Jeffery",
+    lastName: "Cosby",
     age: 50
   };
 
-  display(Object.getOwnPropertyDescriptor(person, 'firstName'));
+  Object.defineProperty(person, "firstName", { enumerable: false });
 
+  for (let propertyName in person) {
+    display(propertyName + ": " + person[propertyName]);
+  }
+
+  display(Object.keys(person));
+
+  display(JSON.stringify(person));
+
+  display(person.firstName);
+
+  // let person = {
+  //   name: {
+  //     firstName: 'Jeffery',
+  //     lastName: 'Cosby',
+  //   },
+  //   age: 50
+  // };
+
+  // Object.defineProperty(person, 'name', { writable: false });
+  // Object.freeze(person.name);
+
+  // // display(Object.getOwnPropertyDescriptor(person, 'firstName'));
+  // // person.firstName = 'Brenna';
+  // person.name.firstName = 'Brenna';
+  // display(person.name);
 })();
