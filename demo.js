f4688c2571d1cf4e7334b5a73943bd91969ad6df
Jeffery Cosby <jcoz1968@gmail.com>
2019-11-17 15:46:16 -0600

    constructor

--- demo.js
+++ demo.js
@@ -1,28 +1,33 @@
 "use strict";
 (function() {
-  let person1 = {
-    firstName: "Jeffery",
-    lastName: "Cosby",
-    age: 18,
-    isAdult() {
-      return this.age >= 18;
-    }
-  };
-
-  let healthStats = {
-    height: 68,
-    weight: 150
-  };
-
-  function mergeHealthStats(person, healthStats) {
-    return Object.assign({}, person1, healthStats);
+  function Person(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.isAdult = function () { return this.age >= 18; }
   }
 
-  let mergedPerson = mergeHealthStats(person1, healthStats);
-  
-  display(mergedPerson);
+  let brenna = new Person('Brenna', 'Cosby', 21);
+  let jeffery = new Person('Jeffery', 'Cosby', 50);
 
-  display(person1);
+  display(brenna.isAdult());
+  display(jeffery.isAdult());
+
+
+  // let healthStats = {
+  //   height: 68,
+  //   weight: 150
+  // };
+
+  // function mergeHealthStats(person, healthStats) {
+  //   return Object.assign({}, person1, healthStats);
+  // }
+
+  // let mergedPerson = mergeHealthStats(person1, healthStats);
+  
+  // display(mergedPerson);
+
+  // display(person1);
 
   // let person2 = {};
   // Object.assign(person2, person1);
