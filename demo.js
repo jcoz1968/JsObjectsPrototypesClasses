c0b97931038d766613de2c9ff5bab175a0a10a43
Jeffery Cosby <jcoz1968@gmail.com>
2019-11-17 17:49:40 -0600

    getters setters in classes

--- demo.js
+++ demo.js
@@ -9,10 +9,21 @@
       this.age = age;
     }
 
+    get fullName() {
+      return this.firstName + " " + this.lastName;
+    }
+
+    set fullName(fullName) {
+      var nameParts = fullName.split(' ');
+      this.firstName = nameParts[0];
+      this.lastName = nameParts[1];
+    }
+
   }
 
   let brenna = new Person('Brenna', 'Cosby', 21);
+  brenna.fullName = 'Marcus Stanfill'
 
-  display(brenna);
+  display(brenna.fullName);
   
 })();
