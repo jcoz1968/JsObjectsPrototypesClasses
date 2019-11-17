e2103c84cedcbbc72455341e79da7e5cd116a62c
Jeffery Cosby <jcoz1968@gmail.com>
2019-11-17 15:22:58 -0600

    inspect object properties

--- demo.js
+++ demo.js
@@ -1,13 +1,17 @@
-'use strict'; 
+"use strict";
 (function() {
-
-   let person = {
-      firstName: 'Jeffery',
-      lastName: 'Cosby', 
-      age: 18,
-      isAdult() { return this.aage >= 18; }
-    };
-
-  display(person.isAdult());
-
+  let person = {
+    firstName: "Jeffery",
+    lastName: "Cosby",
+    age: 18,
+    isAdult() {
+      return this.aage >= 18;
+    }
+  };
+  for (let propertyName in person) {
+    display(propertyName);
+  }
+  // for...in
+  // display(Object.keys(person));
+  // display(person.isAdult());
 })();
