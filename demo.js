891732e73247c845a3bd0fe66c88dd3508373a89
Jeffery Cosby <jcoz1968@gmail.com>
2019-11-17 18:18:52 -0600

    date object

--- demo.js
+++ demo.js
@@ -1,9 +1,12 @@
 "use strict";
 (function () {
 
-  display(Math.PI);
-  display(Math.max(2, 42, 29));
-  display(Math.min(2, 42, 29));
-  display(Math.round(34.5));
-  
+  let date = new Date(1000);
+  let isoDate = new Date('2019-11-17T00:00:00Z');
+  let bestDate = new Date(2017, 10, 17, 1, 30, 50);
+
+  display(date.toString());
+  display(isoDate.toString());
+  display(bestDate.toString());
+
 })();
